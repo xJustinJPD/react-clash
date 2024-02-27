9959191aac4e846d140586820f4a4c25ce863231
Kacper Agatowski <n00212272@iadt.ie>
2024-02-27 15:03:16 +0000

    fixed image problem in create

--- src/teams/Create.js
+++ src/teams/Create.js
@@ -6,21 +6,27 @@ const Create = () => {
     const errorStyle = {
         color: 'red'
     };
-    
+   
     const navigate = useNavigate();
     const [errors, setErrors] = useState({});
     const [form, setForm] = useState({
         name: "",
         size: "",
-        image: "",
-        wins: "",
-        losses: ""
+        image: null
     });
 
     const handleForm = (e) => {
         setForm(prevState => ({
             ...prevState,
             [e.target.name]: e.target.value
+        }));
+    };
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        setForm(prevState => ({
+            ...prevState,
+            image: file
         }));
     };
 
@@ -48,18 +54,27 @@ const Create = () => {
 
     const submitForm = (e) => {
         e.preventDefault();
-        console.log('submitted', form);
+        let token = localStorage.getItem('token');
+        // console.log(token);
+        // console.log('submitted', form);
 
         if(isRequired(['name', 'size'])){
-            let token = localStorage.getItem('token');
-            
+            //created a new form data object
+            let formData = new FormData();
+            //append adds the new data to the associated values
+            formData.append('name', form.name);
+            formData.append('size', form.size);
+            formData.append('image', form.image);
+
             axios.post('http://localhost/api/teams', form, {
                 headers: {
-                    "Authorization": `Bearer ${token}`
+                    "Authorization": `Bearer ${token}`,
+                    //to allow files to the form
+                    "Content-Type": "multipart/form-data"
                 }
             })
             .then(response => {
-                navigate('/courses');
+                navigate('/');
                 console.log('posted')
             })
             .catch(err => {
@@ -87,9 +102,20 @@ const Create = () => {
             <div className="label">
                 <span className="label-text">Size:</span>
             </div>
-            <input type="text" onChange={handleForm} value={form.size} name='size' placeholder="Type here" className="input input-bordered w-full max-w-xs" /><span style={errorStyle}>{errors.size?.message}</span>
+            <input type="number" onChange={handleForm} value={form.size} name='size' placeholder="Type here" className="input input-bordered w-full max-w-xs" /><span style={errorStyle}>{errors.size?.message}</span>
             </label>
             </div>
+            
+            <div className='m-3'>
+            <label className="form-control w-full max-w-xs">
+            <div className="label">
+            <span className="label-text">Team Image</span>
+            <span className="label-text-alt">Image</span>
+            </div>
+            <input type="file" onChange={handleImageChange} name='image' className="file-input file-input-bordered w-full max-w-xs" />
+            </label>
+            </div>
+
 
             <input type='submit' className="btn btn-success m-3" />
             </form>

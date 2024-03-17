486c68cf919bca562bc16a3e1da61a8a3785e49d
xJustinJPD <justinpd97@gmail.com>
2024-03-17 00:22:33 +0000

    added users to teams, and display users in teams

---
+++ src/teams/components/FriendCard.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import AddBtn from './FriendTeam';
+
+const Friend = ({friend, team_id}) => {
+
+	return (
+        <>
+            <div className="card w-96 bg-neutral text-neutral-content">
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{friend.username} </h2>
+                    <b>ID: </b><p>{friend.id}</p> 
+                    <div className="card-actions justify-end">
+                    {/* <Link to={`/teams/${friend.id}`}><button className="btn btn-outline btn-primary">More</button></Link> */}
+                    <AddBtn className="m-3" team_id={team_id} player_username={friend.username} />
+                    </div>
+            </div>
+            </div>
+        </>
+        )
+};
+
+export default Friend;

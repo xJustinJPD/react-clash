207c5b09b88be11cf585e6ca6ba93c034123565a
xJustinJPD <justinpd97@gmail.com>
2024-03-19 17:22:25 +0000

    working

--- src/pages/teams/Create.js
+++ src/pages/teams/Create.js
@@ -1,128 +1,3 @@
-import axios from '../../config/Api';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const Create = () => {
-    const errorStyle = {
-        color: 'red'
-    };
-    
-    const navigate = useNavigate();
-    const [errors, setErrors] = useState({});
-    const [form, setForm] = useState({
-        name: "",
-        size: "",
-        image: ""
-    });
-
-    const handleForm = (e) => {
-        setForm(prevState => ({
-            ...prevState,
-            [e.target.name]: e.target.value
-        }));
-    };
-
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
-        setForm(prevState => ({
-            ...prevState,
-            image: file
-        }));
-    };
-
-    const isRequired = (fields) => {
-
-        let included = true;
-        setErrors({});
-
-        fields.forEach(field => {
-
-            if(!form[field]){
-                included = false;
-                setErrors(prevState => ({
-                    ...prevState,
-                    [field]: {
-                        message: `${field} is required!`
-                    }
-                }));
-            }
-            
-        });
-
-        return included;
-    };
-
-    const submitForm = (e) => {
-        e.preventDefault();
-        let token = localStorage.getItem('token');
-        // console.log(token);
-        // console.log('submitted', form);
-
-        if(isRequired(['name', 'size'])){
-            //created a new form data object
-            let formData = new FormData();
-            //append adds the new data to the associated values
-            formData.append('name', form.name);
-            formData.append('size', form.size);
-            formData.append('image', form.image);
-
-            axios.post('/teams', form, {
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    //to allow files to the form
-                    "Content-Type": "multipart/form-data"
-                }
-            })
-            .then(response => {
-                navigate('/');
-                console.log({form})
-            })
-            .catch(err => {
-                console.error(err);
-            });
-        }
-        
-    };
-    
-    return (
-        <div>
-            <h2 className='m-3'>Create Team</h2>
-            <form onSubmit={submitForm}>
-            <div className='m-3'>
-            <label className="form-control w-full max-w-xs">
-            <div className="label">
-                <span className="label-text">Name:</span>
-            </div>
-            <input type="text" onChange={handleForm} value={form.name} name='name' placeholder="Type here" className="input input-bordered w-full max-w-xs" /><span style={errorStyle}>{errors.name?.message}</span>
-            </label>
-            </div>
-
-            <div className='m-3'>
-            <label className="form-control w-full max-w-xs">
-            <div className="label">
-                <span className="label-text">Size:</span>
-            </div>
-            <input type="number" onChange={handleForm} value={form.size} name='size' placeholder="Type here" className="input input-bordered w-full max-w-xs" /><span style={errorStyle}>{errors.size?.message}</span>
-            </label>
-            </div>
-            
-            <div className='m-3'>
-            <label className="form-control w-full max-w-xs">
-            <div className="label">
-            <span className="label-text">Team Image</span>
-            <span className="label-text-alt">Image</span>
-            </div>
-            <input type="file" onChange={handleImageChange} name='image' className="file-input file-input-bordered w-full max-w-xs" />
-            </label>
-            </div>
-
-            <input type='submit' className="btn btn-success m-3" />
-            </form>
-        </div>
-    );
-};
-
-export default Create;
 import axios from '../config/Api';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -136,8 +11,7 @@ const Create = () => {
     const [errors, setErrors] = useState({});
     const [form, setForm] = useState({
         name: "",
-        size: "",
-        image: ""
+        size: ""
     });
 
     const handleForm = (e) => {
@@ -147,13 +21,13 @@ const Create = () => {
         }));
     };
 
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
-        setForm(prevState => ({
-            ...prevState,
-            image: file
-        }));
-    };
+    // const handleImageChange = (e) => {
+    //     const file = e.target.files[0];
+    //     setForm(prevState => ({
+    //         ...prevState,
+    //         image: file
+    //     }));
+    // };
 
     const isRequired = (fields) => {
 
@@ -189,7 +63,7 @@ const Create = () => {
             //append adds the new data to the associated values
             formData.append('name', form.name);
             formData.append('size', form.size);
-            formData.append('image', form.image);
+            // formData.append('image', form.image);
 
             axios.post('/teams', form, {
                 headers: {
@@ -231,7 +105,7 @@ const Create = () => {
             </label>
             </div>
             
-            <div className='m-3'>
+            {/* <div className='m-3'>
             <label className="form-control w-full max-w-xs">
             <div className="label">
             <span className="label-text">Team Image</span>
@@ -239,7 +113,7 @@ const Create = () => {
             </div>
             <input type="file" onChange={handleImageChange} name='image' className="file-input file-input-bordered w-full max-w-xs" />
             </label>
-            </div>
+            </div> */}
 
             <input type='submit' className="btn btn-success m-3" />
             </form>

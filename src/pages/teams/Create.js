f2bb69caea6f71b3faad857f3b4cd16e2adfc0df
Kacper Agatowski <n00212272@iadt.ie>
2024-03-19 17:17:58 +0000

    route changed when merging

--- src/pages/teams/Create.js
+++ src/pages/teams/Create.js
@@ -1,6 +1,7 @@
 import axios from '../../config/Api';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from '../../config/Api';
 
 const Create = () => {
     const errorStyle = {

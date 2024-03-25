2d58ccdf27ab131e0079d19dc68be13b11dde4b3
Kacper Agatowski <n00212272@iadt.ie>
2024-03-25 19:21:12 +0000

    merge from kacper

--- src/pages/teams/Edit.js
+++ src/pages/teams/Edit.js
@@ -1,10 +1,12 @@
 import { useState, useEffect} from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from '../../config/Api';
+import { useAuth } from "../../contexts/AuthContexts";
 
 const Edit = () => {
     const { id } = useParams();
     const [local] = axios;
+    const { authenticated, userInfo } = useAuth();
     const [team, setTeam] = useState(null);
     const navigate = useNavigate();
     const [errors, setErrors] = useState({});
@@ -88,6 +90,11 @@ const Edit = () => {
             formData.append('size', form.size);
             formData.append('image', form.image);
             formData.append('_method', 'put');
+            if (userInfo && userInfo.role.includes('admin')) {
+                // Append wins and losses only if user is admin
+                formData.append('wins', form.wins);
+                formData.append('losses', form.losses);
+            }
 
 
             local.post(`/teams/${id}`, formData, {
@@ -129,7 +136,26 @@ const Edit = () => {
             <input type="number" onChange={handleForm} value={form.size} name='size' placeholder="Type here" className="input input-bordered w-full max-w-xs" /><span style={errorStyle}>{errors.size?.message}</span>
             </label>
             </div>
-            
+            {authenticated && (userInfo && userInfo.role.includes('admin')) && ( 
+                <>
+                  <div className='m-3'>
+                  <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                      <span className="label-text">Wins:</span>
+                  </div>
+                  <input type="number" onChange={handleForm} value={form.wins} name='wins' placeholder="Type here" className="input input-bordered w-full max-w-xs" /><span style={errorStyle}>{errors.wins?.message}</span>
+                  </label>
+                  </div>
+                    <div className='m-3'>
+                    <label className="form-control w-full max-w-xs">
+                    <div className="label">
+                        <span className="label-text">Losses:</span>
+                    </div>
+                    <input type="number" onChange={handleForm} value={form.losses} name='losses' placeholder="Type here" className="input input-bordered w-full max-w-xs" /><span style={errorStyle}>{errors.losses?.message}</span>
+                    </label>
+                    </div>
+                    </>
+            )}
             <div className='m-3'>
             <label className="form-control w-full max-w-xs">
             <div className="label">

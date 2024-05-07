2ded974e14778b9211f4bfae570272a6f17e44e9
Kacper Agatowski <n00212272@iadt.ie>
2024-05-07 23:47:16 +0100

    username

--- src/pages/Socials/viewProfile.js
+++ src/pages/Socials/viewProfile.js
@@ -154,9 +154,16 @@ const ViewProfile = ({setError}) => {
           <div>
         <Link to={`/user/${user?.id}/edit`}><button className="btn btn-outline btn-lg btn-primary">Edit Profile</button></Link>
       </div>
-      <Link to={'/user/discord-login'}>
+      {discordInfo ? (
+    <>
+        <img width="48" height="48" src="https://img.icons8.com/fluency/48/discord-logo.png" alt="discord-logo"/>
+      <p>{discordInfo.username}</p>
+    </>
+  ) : (
+    <Link to={'/user/discord-login'}>
     <img width="48" height="48" src="https://img.icons8.com/fluency/48/discord-logo.png" alt="discord-logo"/>
   </Link>
+  )}
       <div>
         <Link to={`/user/${user?.id}/password`} ><button className="btn btn-outline btn-lg btn-primary mx-2">Update Password</button></Link>
       </div>

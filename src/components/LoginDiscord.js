81521ffce1509eb4e0215caa6ac6c2528ea7e7bb
Kacper Agatowski <n00212272@iadt.ie>
2024-05-07 12:25:42 +0100

    added padding

--- src/components/LoginDiscord.js
+++ src/components/LoginDiscord.js
@@ -11,7 +11,7 @@ const LoginDiscord = () => {
   };
 
   return (
-    <div className="bg-gray-900 text-white py-20 pb-56">
+    <div className="bg-gray-900 text-white py-20 pb-80">
       <div className="container mx-auto my-auto text-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to Our Application</h1>
         <p className="text-lg mb-8">Connect with Discord to access our awesome features!</p>
@@ -22,7 +22,7 @@ const LoginDiscord = () => {
         >
           Login with Discord
         </button>
-        <p className="mt-4 text-sm">
+        <p className="mt-12 text-sm">
           By clicking "Login with Discord", you agree to allow our application to access your Discord account
           for the purpose of authentication. We take your privacy and data protection seriously. Your Discord
           account information will only be used for authentication and will not be shared with any third parties
